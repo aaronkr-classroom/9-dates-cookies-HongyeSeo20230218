3617e277bf43c34f12f524cd2661b0a46e4ed80a
서홍예 <fnaf0823@gmail.com>
2026-05-19 16:10:48 +0900

    finish

--- c08/datetimezone-object.php
+++ c08/datetimezone-object.php
@@ -1,12 +1,20 @@
 <?php
-// @TODO
+$tz_LDN = new DateTimeZone('Europe/London');
+$tz_SEO = new DateTimeZone('Asia/Seoul');
+$location = $tz_LDN->getLocation();
+
+$LDN     = new Datetime('now', $tz_LDN);
+$SEO     = new Datetime('now', $tz_SEO);
+$SYD     = new Datetime('now', 
+                new DateTimeZone('Australia/Sydney'));
+
 ?>
 <?php include 'includes/header.php'; ?> 
 
 <p><b>LDN: <?= $LDN->format('g:i a') ?></b> 
    (<?= ($LDN->getOffset() / (60 * 60)) ?>)<br>
-   <b>TYO: <?= $TYO->format('g:i a') ?></b>
-   (<?= ($TYO->getOffset() / (60 * 60)) ?>)<br>
+   <b>TYO: <?= $SEO->format('g:i a') ?></b>
+   (<?= ($SEO->getOffset() / (60 * 60)) ?>)<br>
    <b>SYD: <?= $SYD->format('g:i a') ?></b>
    (<?= ($SYD->getOffset() / (60 * 60)) ?>)<br></p>
 
@@ -14,5 +22,9 @@
 <p><?= $tz_LDN->getName() ?><br>
   <b>Longitude:</b> <?= $location['longitude'] ?><br>
   <b>Latitude:</b>  <?= $location['latitude'] ?></p>	
+
+  <pre>
+  <?php var_dump($_COOKIE); ?>
+</pre>
 
 <?php include 'includes/footer.php'; ?>
